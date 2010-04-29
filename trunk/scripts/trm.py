ddec58a5ab60154bbe93b7ae32030c24bbe3dee1
no.author <no.author.@dd566b57-2d5a-cdae-10d7-ce57b7db7bff>
2010-04-29 14:57:45 +0000

    * Fixed problem with reading arguments from commandline.
    * Turned off Message verbosity in loop. This function will have to be moved 
      to project_run.in at a later time. 
    
    * Changed propsetignore to exclude .pyc as well. Started working on trm.py. 
    
    /Bror
    
    
    git-svn-id: http://svn.tracmass.org/trunk@172 2ee5d717-d111-0410-bb1d-d2df4af4126a
    
    git-svn-id: https://github.com/TRACMASS/tracmass.git@142 dd566b57-2d5a-cdae-10d7-ce57b7db7bff

--- trunk/scripts/trm.py
+++ trunk/scripts/trm.py
@@ -1,8 +1,76 @@
 
-from numpy import *
+import numpy as np
+import pylab as pl
+import MySQLdb
+
+class trm:
+    """ main class for TRACMASS data manipulation"""
+    def __init__(self,casename,datadir="/Users/bror/ormOut/", 
+                 ormdir="Users/bror/svn/orm"):
+        self.casename = casename
+        self.datadir = datadir
+        self.ormdir = ormdir
+
+    def read_bin(self, filename):
+        """ Load binary output from TRACMASS """
+        fd = open(filename)
+        runvec = np.fromfile(fd,np.dtype([
+                    ('ints','>i4'), ('ntrac','>i4'), 
+                    ('x','>f4'), ('y','>f4'), ('z','>f4')
+                    ]))
+        return runvec
+
+    def load_to_mysql(self, ints0=0, ftype="run", stype='bin', filename='', 
+                      db="trm", table="traj"):        
+        """Load a tracmass output file and add to mysql table"""
+        if ints0 != 0:
+            filename = "%s%08i_%s.%s" % (self.casename,ints0,ftype,stype)
+        else:
+            ints0 = int(filename[-16:-8])
+        if filename[-3:] == "bin":
+            runtraj = self.read_bin(self.datadir + filename)
+        elif filename[-3:] == "asc":
+            print "Not implemented yet."
+            raise
+        else:
+            print "Unknown file format, data file should be .bin or .asc"
+            raise
+        
+        conn = MySQLdb.connect (host = "localhost",
+                                user = "root",
+                                passwd = "",
+                                db = "trm")        
+        c = conn.cursor ()
+        for r in runtraj:
+            c.execute(
+                """INSERT INTO traj (ints0, ntrac, ints, x, y, z)
+                         VALUES (-999, %s, %s, %s, %s, %s)""",
+                tuple(r) )
+        c.execute("UPDATE traj SET ints0=%i WHERE ints0=-999" % ints0)
+        
+
+        """ Sql commands to create database:
+        CREATE DATABASE trm;
+        CREATE TABLE traj (ints0 INT, ntrac INT, ints INT, 
+                           x FLOAT, y FLOAT, z FLOAT);
+        """
+
+    def sat_to_trajs(self,ints0,field, db="trm", table="traj"):
+        pass
+        
+
+#mpl.dates.num2date(jd30+jd0-1)[0]
+
+
+def test_insert():
+    t = trm('gompom')
+    #t.load_to_mysql(filename="/Users/bror/ormOut/gompom00000033_run.bin")
+    t.load_to_mysql(33)
+    t.load_to_mysql(563)
+
 
 def grid2ll(latMat ,lonMat ,xVec ,yVec):
-
+    
     xMax = a=latMat.shape[1]-2
     yMax = a=latMat.shape[0]-2
 
@@ -56,7 +124,7 @@ def loadtrajs (trajFile):
 
     return ntrac ,ints ,x1 ,y1 ,z1
 
-def diagnostics (gridName,runName):
+#def diagnostics (gridName,runName):
 
     
 
