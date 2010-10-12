54213c2a53fabfad71dea3957ae167d1be24ec4b
(no author) <(no author)@2ee5d717-d111-0410-bb1d-d2df4af4126a>
2010-10-12 18:40:57 +0000

    Added TOPAZ as project.
    git-svn-id: http://svn.tracmass.org/trunk@220 2ee5d717-d111-0410-bb1d-d2df4af4126a

--- scripts/partsat.py
+++ scripts/partsat.py
@@ -1,4 +1,5 @@
 import datetime
+import glob
 
 import numpy as np
 import pylab as pl
@@ -15,7 +16,6 @@ class traj(trm):
     def __init__(self,projname="gmopom",
                  griddir='/projData/GOMPOM/'):
         trm.__init__(self,projname)
-        print dir(trm)
         n = pycdf.CDF(griddir + 'grid.cdf')
         self.llon = n.var('x')[:]    
         self.llat = n.var('y')[:]
@@ -27,14 +27,14 @@ class traj(trm):
             t1str = " AND t.ints=t1.ints "; t2str = ""
         else:
             t2str = " AND t.ints=t2.ints "; t1str = ""
-        class traj: pass
         table = self.tablename + field
         self.c.execute(
             "SELECT DISTINCT(ints) FROM %s WHERE intstart=%i" %
             (table,intstart) )
         ints_t1t2 = self.c.fetchall()
         if len(ints_t1t2) < 2:
-            return False 
+            self.empty = True
+            return
         sql = """
         SELECT t.x as x , t.y as y, t.ntrac as n,
                t1.val as t1, t2.val as t2
@@ -50,59 +50,53 @@ class traj(trm):
         res = zip(*self.c.fetchall())
         if len(res) > 0:
             for n,a in enumerate(['x','y','ntrac','t1','t2']):
-                traj.__dict__[a] = np.array(res[n])
-            traj = self.ijll(traj)
-        return traj
+                self.__dict__[a] = np.array(res[n])
+            self.empty = False
+        else:
+            self.empty = True
+        self.ijll()
 
-    def traj_ncp(self,t):
-        class tr: pass
-        t = int(t)
-        tr_chl  = self.sat_trajs(t, 'chlor_a')
-        tr_chl2 = self.sat_trajs(t, 'chlor_a', pos='end')
-        if not tr_chl: return False
-        tr_k49 = self.sat_trajs(t, 'K_490')
-        msk = np.intersect1d(tr_chl.ntrac,tr_k49.ntrac)
-        tr_k49.msk = [np.flatnonzero(tr_k49.ntrac==m).item()
-                      for m in msk]
-        tr_chl.msk = [np.flatnonzero(tr_chl.ntrac==m).item()
-                      for m in msk]
-        tr_k49.eu1 = float(4.6) / tr_k49.t1
-        tr_k49.eu2 = float(4.6) / tr_k49.t2
-        print len(tr_chl.msk)
-        if len(tr_chl.msk) == 0: return False
-        jds =self.field_jds('chlor_a')
 
-        tr.pc2 = tr_chl.t2[tr_chl.msk] * tr_k49.eu2[tr_k49.msk]*60.
-        tr.pc1 = tr_chl.t1[tr_chl.msk] * tr_k49.eu1[tr_k49.msk]*60.
-        dt = (jds.t2[jds.t0==t]/6. - jds.t1[jds.t0==t]/6.).item()
+    def sat_conc(self,intstart,field,pos='start'):
+        """Retrive fields of start- and end-values""" 
+        jdS = self.field_jds(field)
+        if intstart in jdS.t0:
+            if pos == 'start':
+                ints = jdS.t1[jdS.t0==intstart].item()
+            else:
+                ints = jdS.t2[jdS.t0==intstart].item()
+        else:
+            return self.llat * 0
+        table = self.tablename + field
+        sql = """
+               SELECT round(t.x) x, round(t.y) y, avg(n.val) val
+                  FROM gompom t
+                  INNER JOIN %s n ON
+                     t.intstart=n.intstart AND t.ntrac=n.ntrac
+                  WHERE t.intstart=%i AND n.ints=%s AND t.ints=%s
+                  GROUP BY round(t.x),round(t.y)
+           """ % (table, intstart, intstart+1, ints)
+        n = self.c.execute(sql)
+        fld = self.llat * 0
+        if n == 0: return fld
+        x,y,val = zip(*self.c.fetchall())
+        fld[np.array(y).astype(int),np.array(x).astype(int)-1] = val
+        return fld
 
-        tr.ncp = (tr.pc2-tr.pc1) / dt 
-        tr.ntrac = tr_chl.ntrac[tr_chl.msk]
-        tr.x1 = tr_chl.x[tr_chl.msk]
-        tr.y1 = tr_chl.y[tr_chl.msk]
-        tr.x2 = tr_chl2.x[tr_chl.msk]
-        tr.y2 = tr_chl2.y[tr_chl.msk]
-        tr.lon1 = tr_chl.lon[tr_chl.msk]
-        tr.lat1 = tr_chl.lat[tr_chl.msk]
-        tr.lon2 = tr_chl2.lon[tr_chl.msk]
-        tr.lat2 = tr_chl2.lat[tr_chl.msk]
-        return tr
     
     def sat_field(self,field,ints):
         ds = mpl.dates.num2date(self.ints2iso(ints))
         box8dir = '/projData/JCOOT_sat_Box8/'
         filepre = "A%i%03i" % (ds.year, ds.timetuple().tm_yday)
-        print filepre
         try:
             satfile = glob.glob(box8dir + filepre + "*")[0]
         except IndexError:
             print "Satellite file missing"
             raise
-        print satfile
         h = SD(satfile, SDC.READ)
         return h.select(field)[:]
 
-    def sat_to_db(self,field,intstart,ints,batch=False):
+    def sat_to_db(self,field,intstart,ints,batch=False,traj=False):
         """Load field data to a table. """
         def create_table():
             """Create a mysql table  table """
@@ -114,19 +108,22 @@ class traj(trm):
                     % (self.tablename + field, itp, itp, itp, ftp) )
             self.c.execute(CT)
             self.disable_indexes(self.tablename + field)
-
         create_table()
-        traj = self.trajs(intstart=intstart,ints=ints)
-        if not hasattr(traj,"x"):
-            return False
-        sati,satj = self.gcmij_to_satij(traj)
-        val = self.sat_field(field,ints)[satj,sati]
-        for p in zip(traj.ints[val>=0], traj.ntrac[val>=0], val[val>=0]):
+        if traj:
+            val = traj.val
+        else:
+            traj = self.trajs(intstart=intstart,ints=ints)
+            if not hasattr(traj,"x"):
+                return False
+            sati,satj = self.gcmij_to_satij(traj)
+            val = self.sat_field(field,ints)[satj,sati]
+        print intstart
+        for p in zip(traj.ints[val>-1e9],traj.ntrac[val>-1e9],val[val>-1e9]):
             self.c.execute(
                 "INSERT INTO " + self.tablename + field +
-                " (intstart, ints, ntrac, val) VALUES (-999, %s, %s, %s)", p )
-            self.c.execute("UPDATE %s SET intstart=%i WHERE intstart=-999" % 
-                           (self.tablename + field, intstart) )
+                " (intstart,ints,ntrac,val) VALUES (-999,%s,%s,%s)", p )
+        self.c.execute("UPDATE %s SET intstart=%i WHERE intstart=-999;" 
+                       % (self.tablename + field, intstart) )
         if not batch:
             self.enable_indexes(self.tablename + field)
             
@@ -152,7 +149,81 @@ class traj(trm):
                 ints.__dict__[a] = np.array(res[n])
         return ints
 
+    def median_ncp(self):
+        class svec: pass
+        self.c.execute(
+            """CREATE TEMPORARY TABLE IF NOT EXISTS temp_median 
+                    (id INT AUTO_INCREMENT PRIMARY KEY) 
+                SELECT ints, val FROM gompomncp 
+                ORDER BY ints, val;
+             """)
+        self.c.execute(
+            """CREATE TEMPORARY TABLE temp_median_ids 
+               SELECT ROUND(AVG(id)) AS id FROM temp_median 
+               GROUP BY ints;
+            """)
+        self.c.execute(
+            """SELECT ints, val FROM temp_median_ids 
+               LEFT JOIN temp_median USING (id) ORDER BY ints;
+            """)
+        res = zip(*self.c.fetchall())
+        svec.t1 = res[0]
+        svec.medianNCP = res[1]
+        self.c.execute(
+            """SELECT ints, count(val),max(val),min(val),avg(val) 
+                   FROM gompomncp GROUP BY ints
+                   ORDER BY ints;
+            """)
+        res = zip(*self.c.fetchall())
+        svec.sum   = res[1]
+        svec.max   = res[2]
+        svec.min   = res[3]
+        svec.mean  = res[4]
+        
+        return svec
+
+
 #####################################################################
+def ncp(t, db=False):
+        class tr: pass
+        t = int(t)
+        tr_chl = traj('gompom')
+        tr_chl.sat_trajs(t, 'chlor_a')
+        tr_chl2 = traj('gompom') 
+        tr_chl2.sat_trajs(t, 'chlor_a', pos='end')
+        if tr_chl.empty: return False
+        tr_k49 = traj('gompom') 
+        tr_k49.sat_trajs(t, 'K_490')
+        msk = np.intersect1d(tr_chl.ntrac,tr_k49.ntrac)
+        tr_k49.msk = [np.flatnonzero(tr_k49.ntrac==m).item()
+                      for m in msk]
+        tr_chl.msk = [np.flatnonzero(tr_chl.ntrac==m).item()
+                      for m in msk]
+        tr_k49.eu1 = float(4.6) / tr_k49.t1
+        tr_k49.eu2 = float(4.6) / tr_k49.t2
+        if len(tr_chl.msk) == 0: return False
+        jds =tr_chl.field_jds('chlor_a')
+
+        tr.pc2 = tr_chl.t2[tr_chl.msk] * tr_k49.eu2[tr_k49.msk]*60.
+        tr.pc1 = tr_chl.t1[tr_chl.msk] * tr_k49.eu1[tr_k49.msk]*60.
+        dt = (jds.t2[jds.t0==t]/6. - jds.t1[jds.t0==t]/6.).item()
+
+        tr.ncp = (tr.pc2-tr.pc1) / dt
+        tr.ntrac = tr_chl.ntrac[tr_chl.msk]
+        tr.x1 = tr_chl.x[tr_chl.msk]
+        tr.y1 = tr_chl.y[tr_chl.msk]
+        tr.x2 = tr_chl2.x[tr_chl.msk]
+        tr.y2 = tr_chl2.y[tr_chl.msk]
+        tr.lon1 = tr_chl.lon[tr_chl.msk]
+        tr.lat1 = tr_chl.lat[tr_chl.msk]
+        tr.lon2 = tr_chl2.lon[tr_chl.msk]
+        tr.lat2 = tr_chl2.lat[tr_chl.msk]
+
+        if db:
+            tr.ints=tr.x1*0+t
+            tr.val = tr.ncp
+            tr_chl.sat_to_db('ncp',intstart=t,ints=t,batch=False,traj=tr)
+        return tr
 
 def batch_sat_to_db(field='chlor_a',batchprefix='batch_ints'):
     tr = traj('gompom')
@@ -178,7 +249,6 @@ def calc_ncp_time(batchfile='batch_ints_start.asc'):
     tS.iso = []
     tS.cnt = []
     for t in open(tr.ormdir + "/projects/gomoos/" + batchfile):
-        print t
         ncp = tr.traj_ncp(int(t))
         if not ncp: continue
         tS.ncp.append(np.median(ncp.ncp))
