14994f7a9fa6f7d19777a2f48f6218600061d5fc
jocke <jocke@dd566b57-2d5a-cdae-10d7-ce57b7db7bff>
2011-07-04 08:52:12 +0000

    Trajectories can now be seeded at given time steps. Rewrote and cleaned up in init_seed and seed. Changed some parameters in init_par. /Jocke
    git-svn-id: http://svn.tracmass.org/trunk@247 2ee5d717-d111-0410-bb1d-d2df4af4126a
    
    git-svn-id: https://github.com/TRACMASS/tracmass.git@208 dd566b57-2d5a-cdae-10d7-ce57b7db7bff

--- trunk/src/init_par.f95
+++ trunk/src/init_par.f95
@@ -1,272 +1,315 @@
-subroutine init_params
-  USE mod_param
-  USE mod_seed
-  USE mod_coord
-  USE mod_grid
-  USE mod_name
-  USE mod_time 
-  USE mod_domain
-  USE mod_vel
-  USE mod_traj
-  USE mod_dens
-  USE mod_buoyancy
-  USE mod_streamxy
-  USE mod_streamv
-  USE mod_streamr
-  USE mod_tracer
+SUBROUTINE init_params
+!!------------------------------------------------------------------------------
+!!
+!!
+!!       SUBROUTINE: init_params
+!!
+!!          Loads parameters from the namelists run.in and grid.in
+!!          Allocates matrices for trajectory data, GCM fields,
+!!          and Lagrangian stream functions.
+!!
+!!
+!!       Last change: Joakim Kjellsson, 21 June 2011
+!!
+!!
+!!------------------------------------------------------------------------------
+   USE mod_param
+   USE mod_seed
+   USE mod_coord
+   USE mod_grid
+   USE mod_name
+   USE mod_time 
+   USE mod_domain
+   USE mod_vel
+   USE mod_traj
+   USE mod_dens
+   USE mod_buoyancy
+   USE mod_streamxy
+   USE mod_streamv
+   USE mod_streamr
+   USE mod_tracer
 #if defined diffusion || turb 
-!#ifdef diffusion
-  USE mod_diffusion
+   USE mod_diffusion
 #endif
 #ifdef sediment
-  USE mod_orbital
-  USE mod_sed
+   USE mod_orbital
+   USE mod_sed
 #endif
-  implicit none
+   IMPLICIT NONE
 
-  INTEGER                                    :: argint1 ,argint2
-  INTEGER                                    :: dummy ,factor ,i ,dtstep
-  INTEGER                                    :: gridVerNum ,runVerNum
-  CHARACTER (LEN=30)                         :: inparg, argname
-  CHARACTER (LEN=23)                         ::  Project, Case
+!!------------------------------------------------------------------------------
+   
+   INTEGER                                    ::  argint1 ,argint2
+   INTEGER                                    ::  dummy ,factor ,i ,dtstep
+   INTEGER                                    ::  gridVerNum ,runVerNum
+   CHARACTER (LEN=30)                         ::  inparg, argname
+   CHARACTER (LEN=23)                         ::  Project, Case
 
-  namelist /INITGRIDVER/   gridVerNum
-  namelist /INITGRIDGRID/  IMT, JMT, KM, LBT, NEND
-  namelist /INITGRIDNTRAC/ NTRACMAX
-  namelist /INITGRIDDATE/  yearmin, yearmax, baseSec  ,baseMin  ,baseHour &
-                          ,baseDay  ,baseMon  ,baseYear
-  namelist /INITGRIDTIME/  ngcm, iter, intmax ,fieldsPerFile
-  namelist /INITGRIDARC/   arcscale
-
-  namelist /INITRUNVER/    runVerNum
-  namelist /INITRUNGRID/   subGrid ,subGridImin ,subGridImax ,subGridJmin &
-                          ,subGridJmax ,SubGridFile, subGridID
-  namelist /INITRUNTIME/   intmin, intspin, intrun, intstep 
-  namelist /INITRUNDATE/   startSec ,startMin ,startHour &
-                          ,startDay ,startMon ,startYear &
-                          ,ihour, iday, imon, iyear
-  namelist /INITRUNWRITE/  ncoor, kriva ,inDataDir ,outDataDir, topoDataDir &
-                          ,outDataFile ,intminInOutFile
-  namelist /INITRUNSEED/   nff ,isec ,idir ,nqua ,partQuant ,seedType &
-                          ,ist1 ,ist2 ,jst1 ,jst2 ,kst1 ,kst2 &
-                          ,varSeedFile ,seedDir ,seedFile
-  namelist /INITGRIDDESC/  GCMname,GCMsource,gridName,gridSource &
-                          ,gridDesc  
-  namelist /INITRUNDESC/  caseName ,caseDesc  
+!!------------------------------------------------------------------------------
+!!-------------------- R E A D   F R O M   N A M E L I S T S -------------------
+!!------------------------------------------------------------------------------
+   ! -------------------------------
+   ! --- Parameters from grid.in ---
+   ! -------------------------------
+   namelist /INITGRIDVER/    gridVerNum
+   namelist /INITGRIDDESC/   GCMname, GCMsource, gridName, gridSource, gridDesc
+   namelist /INITGRIDGRID/   IMT, JMT, KM, LBT, NEND
+   namelist /INITGRIDNTRAC/  NTRACMAX
+   namelist /INITGRIDDATE/   yearmin, yearmax, baseSec  ,baseMin  ,baseHour,   &
+                         &   baseDay  ,baseMon  ,baseYear
+   namelist /INITGRIDTIME/   ngcm, iter, intmax ,fieldsPerFile
+   namelist /INITGRIDARC/    arcscale
+   
+   ! ------------------------------
+   ! --- Parameters from run.in ---
+   ! ------------------------------
+   namelist /INITRUNVER/     runVerNum
+   namelist /INITRUNGRID/    subGrid ,subGridImin ,subGridImax ,subGridJmin,   &
+                         &   subGridJmax ,SubGridFile, subGridID
+   namelist /INITRUNTIME/    intmin, intspin, intrun, intstep 
+   namelist /INITRUNDATE/    startSec ,startMin ,startHour,                    &
+                         &   startDay ,startMon ,startYear,                    &
+                         &   ihour, iday, imon, iyear
+   namelist /INITRUNWRITE/   ncoor, kriva,                                     &
+                         &   inDataDir ,outDataDir, topoDataDir,               &
+                             outDataFile ,intminInOutFile
+   namelist /INITRUNSEED/    nff, isec, idir, nqua, partQuant,                 &
+                         &   seedType, seedPos, seedTime, seedAll,             &
+                         &   ist1, ist2, jst1, jst2, kst1, kst2, tst1, tst2,   &
+                             varSeedFile, seedDir, seedFile, timeFile
+   namelist /INITRUNDESC/    caseName, caseDesc  
 #ifdef tempsalt
-  namelist /INITRUNTEMPSALT/ tmin0, tmax0, smin0, smax0, rmin0, rmax0, &
-                             tmine, tmaxe, smine, smaxe, rmine, rmaxe
+   namelist /INITRUNTEMPSALT/ tmin0, tmax0, smin0, smax0, rmin0, rmax0, &
+                         &    tmine, tmaxe, smine, smaxe, rmine, rmaxe
 #endif
 #if defined diffusion || turb 
-!#ifdef diffusion
-  namelist /INITRUNDIFFUSION/ ah, av
+   namelist /INITRUNDIFFUSION/ ah, av
 #endif
 #ifdef sediment
-  namelist /INITRUNSEDIMENT/ partdiam, rhos, cwamp, twave, critvel
+   namelist /INITRUNSEDIMENT/  partdiam, rhos, cwamp, twave, critvel
 #endif
+   namelist /INITRUNEND/ ienw, iene, jens, jenn, timax
 
-
-!  allocate ( ienw (LBT),iene (LBT) )
-!  allocate ( jens (LBT),jenn (LBT) )
-
-  namelist /INITRUNEND/ ienw, iene, jens, jenn, timax
-
-
-  Project  = PROJECT_NAME
-  Case     = CASE_NAME
+!!------------------------------------------------------------------------------   
+   
+   Project  = PROJECT_NAME
+   Case     = CASE_NAME
+   
+   IF ( (IARGC() == 1 ) .OR. (IARGC() == 4 ) )  then
+      CALL getarg(IARGC(),inparg)
+      Case = inparg
+   END IF
   
-  if ( (IARGC() .eq. 1 ) .or. (IARGC() .eq. 4 ) )  then
-     call getarg(IARGC(),inparg)
-     Case=inparg
-  end if
+   OPEN (8,file='projects/'//trim(Project)//'/'//trim(Project)//'_grid.in',    &
+       & status='OLD', delim='APOSTROPHE')
+   
+      ! -- Check if the namefiles has correct version number. 
+      READ (8,nml=INITGRIDVER)
+         IF (gridVerNum < 1) THEN
+            PRINT *,'==================== ERROR ===================='
+            PRINT *,'Your grid namefile seems to be out of date.'
+            PRINT *,'Check the version_grig.txt file for changes.'
+            PRINT *,'You have to edit the version number in you grid'
+            PRINT *,'manually when done.'
+            STOP
+         END IF
+      READ (8,nml=INITGRIDDESC)
+      READ (8,nml=INITGRIDGRID)
+      READ (8,nml=INITGRIDNTRAC)
+      READ (8,nml=INITGRIDTIME)
+      READ (8,nml=INITGRIDDATE)
+      READ (8,nml=INITGRIDARC)
+   
+   CLOSE (8)
+      
+      OPEN (8,file='projects/'//trim(Project)//'/'//trim(Case)//'_run.in',     &
+          & status='OLD', delim='APOSTROPHE')
+         
+         READ (8,nml=INITRUNDESC)
+         READ (8,nml=INITRUNGRID)
+         SELECT CASE (subGrid)
+         
+         CASE (0)
+            
+            PRINT *,'Use the Full grid.'     
+            subGridImin =   1
+            subGridJmin =   1
+            subGridImax = imt
+            subGridJmax = jmt
+       
+         CASE (1)
+    
+            PRINT *,'Use a subgrid: ', subGridImin ,subGridImax, &
+                &   subGridJmin ,subGridJmax
+            imt = subGridImax-subGridImin+1
+            jmt = subGridJmax-subGridJmin+1
+       
+         CASE default
+    
+            PRINT *,'==================== ERROR ===================='
+            PRINT *,'This subGrid selection is not implemented yet.'
+            PRINT *,'subGrid = ' ,subGrid
+            STOP
+         
+         END SELECT
   
-  ! -- Check if there is a time argument and if so, use it.
-!  print *,trim(Project)//'/'//trim(Case)//'_grid.in'
-  open(8,file='projects/'//trim(Project)//'/'//trim(Project)//'_grid.in',  &
-       status='OLD', delim='APOSTROPHE')
-  ! -- Check if the namefiles has correct version number. 
-  read(8,nml=INITGRIDVER)
-  if (gridVerNum < 1) then
-     print *,'==================== ERROR ===================='
-     print *,'Your grid namefile seems to be out of date.'
-     print *,'Check the version_grig.txt file for changes.'
-     print *,'You have to edit the version number in you grid'
-     print *,'manually when done.'
-     stop
-  end if
-  read(8,nml=INITGRIDDESC)
-  read(8,nml=INITGRIDGRID)
-  read(8,nml=INITGRIDNTRAC)
-  read(8,nml=INITGRIDTIME)
-  read(8,nml=INITGRIDDATE)
-  !=== === ===
-  read(8,nml=INITGRIDARC)
-  open(8,file='projects/'//trim(Project)//'/'//trim(Case)//'_run.in',  &
-       status='OLD', delim='APOSTROPHE')
-  read(8,nml=INITRUNDESC)
-  read(8,nml=INITRUNGRID)
-  select case (subGrid)
-  case (0)
-     print *,'Use the Full grid.'     
-     subGridImin =   1
-     subGridJmin =   1
-     subGridImax = imt
-     subGridJmax = jmt
-  case (1)
-     print *,'Use a subgrid: ', subGridImin ,subGridImax &
-          ,subGridJmin ,subGridJmax
-     imt=subGridImax-subGridImin+1
-     jmt=subGridJmax-subGridJmin+1
-  case default
-     print *,'==================== ERROR ===================='
-     print *,'This subGrid selection is not implemented yet.'
-     print *,'subGrid = ' ,subGrid
-     stop
-  end select
-  !=== === ===
-  read(8,nml=INITRUNTIME)
-  read(8,nml=INITRUNDATE)
-  read(8,nml=INITRUNWRITE)  
-  read(8,nml=INITRUNSEED)
+         READ (8,nml=INITRUNTIME)
+         READ (8,nml=INITRUNDATE)
+         READ (8,nml=INITRUNWRITE)  
+         READ (8,nml=INITRUNSEED)
 #ifdef tempsalt
-  read(8,nml=INITRUNTEMPSALT)
+         READ (8,nml=INITRUNTEMPSALT)
 #endif
 #if defined diffusion || turb 
-!#ifdef diffusion
-  read(8,nml=INITRUNDIFFUSION)
+         READ (8,nml=INITRUNDIFFUSION)
 #endif
 #ifdef sediment
-  read(8,nml=INITRUNSEDIMENT)
+         READ (8,nml=INITRUNSEDIMENT)
 #endif
+         READ(8,nml=INITRUNEND)
+         
+      CLOSE (8)
 
-  read(8,nml=INITRUNEND)
+      timax   =  24.*3600.*timax ! convert time lengths from days to seconds
+      dstep   =  1.d0/dble(iter)
+      dtmin   =  dtstep*tseas
 
-  timax=24.*3600.*timax ! convert time lengths from days to seconds
-  dstep=1.d0/dble(iter)
-  dtmin=dtstep*tseas
-
-  baseJD      = jdate(baseYear  ,baseMon  ,baseDay)
-  startJD     = jdate(startYear ,startMon ,startDay)
-  startYearCond: if (startYear .ne. 0) then
-     if(ngcm.ge.24) then 
-        intmin      = (startJD-baseJD)/(ngcm/24)+1
-     else ! this is a quick fix to avoid division by zero when ngcm < 24
-        intmin      = (startJD-baseJD-ngcm)/(ngcm)+1
-     endif
-  end if startYearCond
-  startHourCond: if ( (startHour .ne. 0)  & 
-       .or. (startMin  .ne. 0)  &
-       .or. (startSec  .ne. 0) ) then
-     print *,'------------------------------------------------------'
-     print *,'ERROR!'
-     print *,'------------------------------------------------------'
-     print *,'Fractions of day for start values not implemented yet.'
-     print *,'Email Bror (brorfred@gmail.com) to have it fixed.'
-     stop 100
-  end if startHourCond
+      baseJD   =  jdate(baseYear  ,baseMon  ,baseDay)
+      startJD  =  jdate(startYear ,startMon ,startDay)
+      
+      startYearCond: IF (startYear /= 0) THEN
+         IF (ngcm >= 24) THEN 
+            intmin      = (startJD-baseJD)/(ngcm/24)+1
+         ELSE ! this is a quick fix to avoid division by zero when ngcm < 24
+            intmin      = (startJD-baseJD-ngcm)/(ngcm)+1
+         END IF
+      END IF startYearCond
+      
+      startHourCond: IF ((startHour /= 0) .OR. &
+             &           (startMin  /= 0) .OR. &
+             &           (startSec  /= 0)      ) THEN
+         PRINT *,'------------------------------------------------------'
+         PRINT *,'ERROR!'
+         PRINT *,'------------------------------------------------------'
+         PRINT *,'Fractions of day for start values not implemented yet.'
+         PRINT *,'Email Bror (brorfred@gmail.com) to have it fixed.'
+         STOP 100
+      END IF startHourCond
   
-  if ((IARGC() > 1) )  then
-     call getarg(2,inparg)
-     factor=1
-     argint1=0
-     do i=29,1,-1
-        if (ichar(inparg(i:i)) .ne. 32) then
-           argint1=argint1+(ichar(inparg(i:i))-48)*factor
-           factor=factor*10
-        end if
-     end do
-     ARG_INT1=argint1
-  end if
-  if ((IARGC() > 2) )  then
-     call getarg(3,inparg)
-     factor=1
-     argint2=0
-     do i=29,1,-1
-        if (ichar(inparg(i:i)) .ne. 32) then
-           argint2=argint2+(ichar(inparg(i:i))-48)*factor
-           factor=factor*10
-        end if
-     end do
-     ARG_INT2=argint2
-  end if
- 
- 
-  tseas= dble(ngcm)*3600.d0 ! time step between data sets
+      IF ((IARGC() > 1) )  THEN
+         CALL getarg(2,inparg)
+         factor=1
+         argint1=0
+         DO i=29,1,-1
+            IF (ichar(inparg(i:i)) /= 32) THEN
+               argint1=argint1+(ichar(inparg(i:i))-48)*factor
+               factor=factor*10
+            END IF
+         END DO
+         ARG_INT1=argint1
+      END IF
+    
+      IF ((IARGC() > 2) ) THEN
+         CALL getarg(3,inparg)
+         factor=1
+         argint2=0
+         DO i=29,1,-1
+            IF (ichar(inparg(i:i)) /= 32) THEN
+               argint2=argint2+(ichar(inparg(i:i))-48)*factor
+               factor=factor*10
+            END IF
+         END DO
+         ARG_INT2=argint2
+      END IF
+      
+      ! tseas - the time step between data sets in [s]
+      tseas= dble(ngcm)*3600.d0
 
+      ! --- ist -1 to imt ---
+      IF ( ist1 == -1) THEN 
+         ist1 = IMT
+      END IF
+      IF ( ist2 == -1) THEN 
+         ist2 = IMT
+      END IF
+      
+      ! --- jst -1 to jmt ---
+      IF ( jst1 == -1) THEN
+         jst1=jmt
+      END IF
+      IF ( jst2 == -1) THEN
+         jst2=jmt
+      END IF
+    
+      ! --- kst -1 to km ---
+      IF ( kst1 == -1) THEN
+         kst1 = KM
+      END IF
+      IF ( kst2 == -1) THEN 
+         kst2 = KM
+      END IF
 
-  ! --ist -1 to imt
-  if ( ist1 == -1) then 
-     ist1=imt
-  end if
-  if ( ist2 == -1) then 
-     ist2=imt
-  end if
-  ! --jst -1 to jmt
-  if( jst1 == -1) then 
-     jst1=jmt
-  end if
-  if ( jst2 == -1) then 
-     jst2=jmt
-  end if
-  ! --ist -1 to imt  
-  if ( kst1 == -1) then 
-     kst1=km
-  end if
-  if ( kst2 == -1) then 
-     kst2=km
-  end if
+!!------------------------------------------------------------------------------
+!!------------------------ A L L O C A T I O N S -------------------------------
+!!------------------------------------------------------------------------------
 
-  ! mod_coord
-  allocate ( csu (0:jmt), cst(jmt)  ) 
-  allocate ( phi(0:jmt),   zw(0:km) ) 
-  allocate ( dyt(jmt), dxv(imt+2,jmt), dyu(imt+2,jmt) ) 
-  ! mod_grid
+      ! --- Allocate information about the coordinates and grid ---
+
+      ALLOCATE ( csu (0:jmt), cst(jmt)  ) 
+      ALLOCATE ( phi(0:jmt),   zw(0:km) ) 
+      ALLOCATE ( dyt(jmt), dxv(imt+2,jmt), dyu(imt+2,jmt) ) 
 #ifdef zgrid3Dt
-  allocate ( dzt(imt,jmt,km,nst) )   
+      ALLOCATE ( dzt(imt,jmt,km,nst) )   
 #elif  zgrid3D
-  allocate ( dzt(imt,jmt,km) )   
+      ALLOCATE ( dzt(imt,jmt,km) )   
 #endif /*zgrid3Dt*/
 #ifdef varbottombox
-  allocate ( dztb(imt,jmt,nst) )   
+      ALLOCATE ( dztb(imt,jmt,nst) )   
 #endif /*varbottombox*/
-  allocate ( dxdy(imt,jmt) )   
-  allocate (kmt(imt,jmt), dz(km) )
-  ! mod_domain
-  !allocate ( ienw (LBT),iene (LBT) )
-  !allocate ( jens (LBT),jenn (LBT) )
-  allocate ( uflux(imt,jmt,km,nst), vflux(imt,0:jmt,km,nst) )
-  allocate ( hs(imt+1,jmt+1,nst) )
+      ALLOCATE ( dxdy(imt,jmt) )   
+      ALLOCATE ( kmt(imt,jmt), dz(km) )
+    
+      ! --- Allocate velocity fields, temperature, salinity, density, --- 
+      ! --- sea-surface height, and trajectory data                   ---
+      
+      ALLOCATE ( uflux(imt,jmt,km,nst), vflux(imt,0:jmt,km,nst) )
+      ALLOCATE ( hs(imt+1,jmt+1,nst) )
 #ifdef full_wflux
-  allocate ( wflux(imt+2 ,jmt+2 ,0:km ,2) )
+      ALLOCATE ( wflux(imt+2 ,jmt+2 ,0:km ,2) )
 #else
-  allocate ( wflux(0:km,2) )
+      ALLOCATE ( wflux(0:km,2) )
 #endif
-  allocate ( uvel(imt+2,jmt,km) ,vvel(imt+2,jmt,km) ,wvel(imt+2,jmt,km) )
-  allocate ( trj(ntracmax,NTRJ), nrj(ntracmax,NNRJ) )
-  ! mod_dens
+      ALLOCATE ( uvel(imt+2,jmt,km) ,vvel(imt+2,jmt,km) ,wvel(imt+2,jmt,km) )
+      ALLOCATE ( trj(ntracmax,NTRJ), nrj(ntracmax,NNRJ) )
 #ifdef tempsalt
-  allocate ( tem(imt,jmt,km,nst) ,sal(imt,jmt,km,nst), rho(imt,jmt,km,nst) )
+      ALLOCATE ( tem(imt,jmt,km,nst) ,sal(imt,jmt,km,nst), rho(imt,jmt,km,nst) )
 #endif
-  ! mod_streamxy
+
+      ! --- Allocate Lagrangian stream functions ---
+
 #ifdef streamxy
-  allocate ( stxyy(imt,jmt,lbt), stxyx(imt,jmt,lbt) )
+      ALLOCATE ( stxyy(imt,jmt,lbt), stxyx(imt,jmt,lbt) )
 #endif
-  ! mod_streamv
 #ifdef streamv
-  allocate ( stxz(imt,km,lbt), styz(jmt,km,lbt) )
+      ALLOCATE ( stxz(imt,km,lbt), styz(jmt,km,lbt) )
 #endif
-  ! mod_streamr
 #ifdef streamr
-  allocate ( stxr(imt,mr,lbt,lov), styr(jmt,mr,lbt,lov) )
+      ALLOCATE ( stxr(imt,mr,lbt,lov), styr(jmt,mr,lbt,lov) )
 #endif
-! mod_tracer
+
+      ! --- Allocate tracer data ---
+
 #ifdef tracer
-  allocate ( tra(imt,jmt,km) )
+      ALLOCATE ( tra(imt,jmt,km) )
 #endif
-  ! mod_sed
+
+      ! --- Allocate sedimentation data ---
+
 #ifdef sediment
-  allocate (orb(km) )
+      ALLOCATE (orb(km) )
 #endif
 
-end subroutine init_params
+END SUBROUTINE init_params
 
+!!------------------------------------------------------------------------------
+!!------------------------------------------------------------------------------
