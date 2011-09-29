f579ca5d2eac65144f9492a0f3d4755badd443f3
bror <bror@dd566b57-2d5a-cdae-10d7-ce57b7db7bff>
2011-09-29 13:58:11 +0000

    The jpl NOWCAST project now working.
    git-svn-id: http://svn.tracmass.org/trunk@253 2ee5d717-d111-0410-bb1d-d2df4af4126a
    
    git-svn-id: https://github.com/TRACMASS/tracmass.git@214 dd566b57-2d5a-cdae-10d7-ce57b7db7bff

--- trunk/projects/jplNOW/readfield.f95
+++ trunk/projects/jplNOW/readfield.f95
@@ -30,119 +30,73 @@ SUBROUTINE readfields
   ! = Variables used for getfield procedures
   CHARACTER (len=200)                        :: gridFile ,fieldFile
   CHARACTER (len=50)                         :: varName
-  
-  ! = ECCO Grid fields
-  REAL, SAVE, ALLOCATABLE, DIMENSION(:)      :: valsz
-  REAL, SAVE, ALLOCATABLE, DIMENSION(:,:)    :: e1v ,e1t ,e2u ,e2t
-  REAL, DIMENSION(2)                         :: ttest1, ttest2
-  
+
+  ! = Variables for converting from S to Z
+  REAL,       ALLOCATABLE, DIMENSION(:)      :: sc_r,Cs_r
+  INTEGER                                    :: hc
+
   ! = Input fields from GCM
-  REAL,       ALLOCATABLE, DIMENSION(:,:)    :: ssh
+  REAL,       ALLOCATABLE, DIMENSION(:,:)    :: ssh,dzt0
   ! ===   ===   ===
-  
-  alloCondGrid: if(.not. allocated (e1v)) then
-     allocate ( valsz(km) )
-     allocate ( e1v(imt+2,jmt)   ,e1t(imt+2,jmt) )
-     allocate ( e2u(imt+2,jmt)   ,e2t(imt+2,jmt) )
-  end if alloCondGrid
+
   
   alloCondUVW: if(.not. allocated (ssh)) then
-     allocate ( ssh(imt,jmt) )
+     allocate ( ssh(imt,jmt), dzt0(imt,jmt) )
+     allocate ( sc_r(km), Cs_r(km) )
   end if alloCondUVW
   alloCondDZ: if(.not. allocated (dzu)) then
      allocate ( dzu(imt,jmt,km), dzv(imt,jmt,km) )
   end if alloCondDZ
   ! ===   ===   ===   ===   ===   ===   ===   ===   ===   ===   ===
-  
-  call datasetswap !Copy field(t+1) to field(t).
+  sc_r = 0
+  Cs_r = 0
+
+
+  call datasetswap
   call updateClock
 
   ! === update the time counting ===
   intpart1    = mod(ints,24)
   intpart2    = floor((ints)/24.)
-  dstamp      = 'scb_das_0000000000.nc'
+  dstamp      = '2011083121_da.nc'
 
-  write (dstamp(9:18),'(i4i2.2i2.2i2.2)') & 
+  write (dstamp(1:10),'(i4i2.2i2.2i2.2)') & 
        currYear,currMon,currDay,currHour+3
-  dataprefix  = trim(inDataDir) // '/ROMS/' // dstamp
+  dataprefix  = trim(inDataDir) // dstamp
   tpos        = intpart1+1
+
   print *,dataprefix
-  ! === initialise ===
-  initCond: if(ints.eq.intstart) then
-     ! call coordinat
-     ndates = 0
-     hs     = 0.
-     uflux  = 0.
-     vflux  = 0.
-#ifdef tempsalt
-     tem    = 0.
-     sal    = 0.
-     rho    = 0.
-#endif
-     
-     ! ======================================================
-     !    ===  Set up the grid ===
-     ! ======================================================
-     zw(0:km-1) = get1DfieldNC(trim(dataprefix) ,'depth')
-     zw(km)     = 2500
-     dz         = zw(km:1:-1)-zw(km-1:0:-1)
-     
-     ! ### FUSK! ###
-     e2t=1153.4
-     e2u=e2t
-     e1t=2226.9
-     e1v=e1t
-
-     dxdy=e1t*e2t
-     
-     do k=1,km
-        dzt(:,:,k)=dz(k)
-     end do
-     dzu=dzt
-     dzv=dzt
-
-     ncTpos=1
-     uvel = get3DfieldNC(trim(dataprefix) ,   'temp')
-
-     kmt  = 0 
-     do j=1,jmt
-        do i=1,IMT
-           do k=1,km
-              kk=km+1-k
-              if(uvel(i,j,k).ne.-9999) kmt(i,j)=k
-           end do
-           if(kmt(i,j).ne.0) then
-              dztb(i,j,1)=dzt(i,j,kmt(i,j))
-           else
-              dztb(i,j,1)=0.
-           endif
-        end do
-     end do
-
-
-  endif initCond   ! === End init section ===
 
   uvel      = get3DfieldNC(trim(dataprefix) ,   'u')
   vvel      = get3DfieldNC(trim(dataprefix) ,   'v')
   ssh       = get2dfieldNC(trim(dataprefix) ,'zeta')
   hs(:,:,2) = ssh
 
-  where (uvel .eq. -9999) uvel=0
-  where (vvel .eq. -9999) vvel=0
-  where (ssh  .eq. -9999)  ssh=0
-
-  dzu(1:imt-1,:,1) = dzu(1:imt-1,:,1)+0.5*(hs(1:imt-1,:,2)+hs(2:imt,:,2))
-  dzv(:,1:jmt-1,1) = dzv(:,1:jmt-1,1)+0.5*(hs(:,1:jmt-1,2)+hs(:,2:jmt,2))
-  dzu(imt,:,1)     = dzu(imt,:,1)    +hs(imt,:,2)
-  dzv(:,jmt,1)     = dzv(:,jmt,1)    +hs(:,jmt,2)
-  
+  ierr = NF90_OPEN(trim(dataprefix) ,NF90_NOWRITE ,ncid)
+  ierr = NF90_GET_ATT(ncid, NF90_GLOBAL, 'sc_w', sc_r)
+  ierr = NF90_GET_ATT(ncid, NF90_GLOBAL, 'Cs_w', Cs_r)
+  ierr = NF90_GET_ATT(ncid, NF90_GLOBAL, 'hc', hc)
   do k=1,km
-     kk=km+1-k
-     uflux(:,:,k,2)   = uvel(:,:,kk) * e2u * dzu(:,:,k)
-     vflux(:,:,k,2)   = vvel(:,:,kk) * e1v * dzv(:,:,k)
+     dzt0 = (sc_r(k)-Cs_r(k))*hc + Cs_r(k) * depth
+     dzt(:,:,k)= dzt0 + ssh*(1.0 + dzt0/depth)
   end do
 
- return
+  dzt0 = dzt(:,:,km)
+  dzt(:,:,1:km-1)=dzt(:,:,2:km)-dzt(:,:,1:km-1)
+  dzt(:,:,km) = ssh-dzt0
+
+
+
+  dzu(1:imt-1,:,:) = dzt(1:imt-1,:,:)*0.5 + dzt(2:imt,:,:)*0.5
+  dzv(:,1:jmt-1,:) = dzt(:,1:jmt-1,:)*0.5 + dzt(:,2:jmt,:)*0.5
+
+  do k=1,km
+     kk=km+1-k
+     uflux(:,:,k,2)   = uvel(:,:,k) * dzu(:,:,k) * dyu
+     vflux(:,:,k,2)   = vvel(:,:,k) * dzv(:,:,k) * dxv
+  end do
+  return
+
 
  !===   ===   ===   ===   ===   ===   ===   ===   ===   ===   ===
 
