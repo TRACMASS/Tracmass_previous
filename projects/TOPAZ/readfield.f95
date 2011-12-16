9ef38009968dca5d993a7403a165293b25b07a73
doos <doos@2ee5d717-d111-0410-bb1d-d2df4af4126a>
2011-12-16 15:40:58 +0000

    git-svn-id: http://svn.tracmass.org/trunk@276 2ee5d717-d111-0410-bb1d-d2df4af4126a

--- projects/TOPAZ/readfield.f95
+++ projects/TOPAZ/readfield.f95
@@ -18,8 +18,8 @@ SUBROUTINE readfields
   IMPLICIT none
 
   CHARACTER(len=200), SAVE         :: ncFile, maskfile
-  INTEGER                          :: i, im, ip, j, jm, jp, k, kk
-  INTEGER                          :: year, month, day, nread, ndates
+  INTEGER                          :: i, im, ip, j, jm, jp, k, kk, ii
+  INTEGER                          :: nread, ndates
   INTEGER, SAVE                    :: varid_u,varid_v,varid_ssh,varid_sst,varid_sss,varid_rho
   LOGICAL                          :: around
   CHARACTER(len=100)               :: fileName
@@ -39,13 +39,30 @@ END IF alloCondUVW
 
 
   call datasetswap
-
+  
+! initialise  
+  if(ints.eq.1) then
+   uflux=0. ; vflux=0.
+   imon=0 ; iyear=startYear 
+  endif
+  
+  ! time count
+  if(imon.eq.0) iyear=startYear
+  imon=imon+1
+  if(imon.eq.13) then
+   imon=1
+   iyear=iyear+1
+   if(iyear.eq.2008) iyear=2006
+  endif
+ 
   ! === NetCDF file and fields ===
-  fileName = 'monthlydata/ocean-20071231_month.nc'
+  fileName = ''
+  write(fileName(1:4), '(i4) ') iyear
+  fileName = 'monthlydata/ocean_month_'//trim(fileName)//'1231.nc'
   ncFile   = trim(inDataDir)//fileName
-  nread=mod(ints,2)+1
-  nread=1
-!  print *,'ncFile=',ints,nread,ncFile
+  nread=mod(ints,12)+1
+  nread=imon
+!  print *,'ncFile=',imon,iyear,ints,nread,ncFile
 
   map2D    = [  1, 2, 3, 4 ]  
   map3D    = [  1, 2, 3, 4 ]  
@@ -65,7 +82,7 @@ END IF alloCondUVW
       PRINT*,NF90_STRERROR (ierr)
       STOP
    END IF
-   ierr = NF90_INQ_VARID (ncid,'eta_u',varid_ssh)
+   ierr = NF90_INQ_VARID (ncid,'eta_t',varid_ssh)
    IF (ierr /= 0) THEN
       PRINT*,NF90_STRERROR (ierr)
       STOP
@@ -117,18 +134,31 @@ END IF alloCondUVW
    END DO
 
    ! Read U velocity
-   ierr = NF90_GET_VAR(ncid,varid_u,uvel,start3d,count3d)
+   ierr = NF90_GET_VAR(ncid,varid_u,temp3d_simp,start3d,count3d)
    IF (ierr /= 0) THEN
       PRINT*,NF90_STRERROR (ierr)
       STOP
    END IF
    
+     ! === Velocities ===
+
+  where (temp3d_simp<-1000.) 
+   temp3d_simp = 0.
+  end where
+  uvel(1:IMT,:,:)=temp3d_simp
+
+   
    ! Read V velocity
-   ierr = NF90_GET_VAR(ncid,varid_v,vvel,start3d,count3d)
+   ierr = NF90_GET_VAR(ncid,varid_v,temp3d_simp,start3d,count3d)
    IF (ierr /= 0) THEN
       PRINT*,NF90_STRERROR (ierr)
       STOP
    END IF
+   
+  where (temp3d_simp<-1000.) 
+   temp3d_simp = 0.
+  end where
+  vvel(1:IMT,:,:)=temp3d_simp
    
 #ifdef tempsalt
    ! Read SST 
@@ -175,14 +205,14 @@ END IF alloCondUVW
 !      STOP
 !   END IF
 
-  ! === Velocities ===
 
-  where (uvel<-1000.) 
-   uvel = 0.
-  end where
-  where (vvel<-1000.) 
-   vvel = 0.
-  end where
+  
+!  dyu=1000000. ; dxv=1000000. 
+  
+!  do j=JMT,1,-1
+! write(*,"(370i1)") (int(100000.*uvel(i,j,1)),i=1,IMT/2)
+!enddo 
+!stop 3495
 
   do i=1,IMT
    ip=i+1
@@ -194,7 +224,7 @@ END IF alloCondUVW
     jm=j-1
     if(j.eq.JMT) jp=JMT
     if(j.eq.  1) jm=1
-    do k=1,km
+    do k=1,KM
      kk=km-k+1
      du=dzu(i,j,k)
      dv=dzv(i,j,k)
@@ -202,8 +232,26 @@ END IF alloCondUVW
       du=du+0.5*(hs(i,j,2)+hs(ip,j,2))
       dv=dv+0.5*(hs(i,j,2)+hs(i,jp,2))
      endif
-     uflux(i,j,k,2) = 0.5 * (uvel(i,j,kk)+uvel(im,j,kk)) * dyu(i,j) * du
-     vflux(i,j,k,2) = 0.5 * (vvel(i,j,kk)+vvel(i,jm,kk)) * dxv(i,j) * dv
+!     if(du.lt.0. .and. kk.le.kmu(i,j)) then
+!      print *,i,j,k,dzu(i,j,k),du
+!      print *,'hs',hs(i,j,2),hs(ip,j,2)
+!      print *,'uv',uvel(i,j,kk),uvel(i,jm,kk)
+!      write(*,"(370i1)") (kmt(ii,jp),ii=im,ip)
+!      write(*,"(370i1)") (kmt(ii,j),ii=im,ip)
+!      write(*,"(370i1)") (kmt(ii,jm),ii=im,ip)
+!      stop 4967
+!     endif
+!      if(dv.lt.0. .and. kk.le.kmv(i,j)) then
+!      print *,i,j,k,dzv(i,j,k),dv
+!      print *,'hs',hs(i,j,2),hs(i,jp,2)
+!      print *,'uv',uvel(i,j,kk),uvel(i,jm,kk)
+!      write(*,"(370i1)") (kmt(ii,jp),ii=im,ip)
+!      write(*,"(370i1)") (kmt(ii,j),ii=im,ip)
+!      write(*,"(370i1)") (kmt(ii,jm),ii=im,ip)
+!      stop 4968
+!     endif
+     if(kk.le.kmu(i,j)) uflux(i,j,k,2) = 0.5 * (uvel(i,j,kk)+uvel(i,jm,kk)) * dyu(i,j) * du
+     if(kk.le.kmv(i,j)) vflux(i,j,k,2) = 0.5 * (vvel(i,j,kk)+vvel(im,j,kk)) * dxv(i,j) * dv
     enddo
    enddo
   enddo
