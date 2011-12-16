5bebaab12973e1bcc6274d107822c8acd74f8c10
doos <doos@dd566b57-2d5a-cdae-10d7-ce57b7db7bff>
2011-12-16 15:47:41 +0000

    restoring topaz
    git-svn-id: http://svn.tracmass.org/trunk@277 2ee5d717-d111-0410-bb1d-d2df4af4126a
    
    git-svn-id: https://github.com/TRACMASS/tracmass.git@236 dd566b57-2d5a-cdae-10d7-ce57b7db7bff

--- trunk/projects/TOPAZ/setupgrid.f95
+++ trunk/projects/TOPAZ/setupgrid.f95
@@ -1,9 +1,9 @@
 SUBROUTINE setupgrid
   
   USE netcdf
-  USE mod_coord
   USE mod_param
   USE mod_vel
+  USE mod_coord
   USE mod_time
   USE mod_grid
   USE mod_name
@@ -34,179 +34,62 @@ SUBROUTINE setupgrid
   ! -------------------------------------------------------------
 
   ! === Init local variables for the subroutine ===
-  INTEGER                                    :: i ,j ,k ,kk, im, jm, ip, jp
-!  INTEGER, SAVE, ALLOCATABLE, DIMENSION(:,:) :: mask
+  INTEGER                                    :: i ,j ,k ,kk
+  INTEGER, SAVE, ALLOCATABLE, DIMENSION(:,:) :: mask
   CHARACTER (len=200)                        :: gridfile
-  REAL tem3d(imt,jmt,km),tem2d(imt,jmt)
-  REAL geolon(IMT,JMT),geolat(IMT,JMT)
-  INTEGER itemp2d(IMT,JMT)
-  logical                                    :: around
 
   allocate ( dzu(imt,jmt,km),dzv(imt,jmt,km),dzt0surf(imt,jmt) )
-  allocate ( depth(imt,jmt),  mask(imt,jmt), kmu(imt,jmt), kmv(imt,jmt) )
-  
-  
+  allocate ( depth(imt,jmt),  mask(imt,jmt) )
   call coordinat
 
-gridfile = trim(inDataDir) // 'grid_auscom.nc'
-print *,trim(gridfile),' tday=',tday
-
-  map2D    = [  1, 2, 3, 4 ]  
-  map3D    = [  1, 2, 3, 4 ]  
+  gridfile = trim(inDataDir) // '../grid_spec_cm2_ocean_tripolar.nc'
+   
   start1d  = [  1]
-  count1d  = [ KM]
-  start2d  = [1  ,   1,  1, 1 ]
-  count2d  = [IMT, JMT,  1, 1 ]
-  start3d  = [1  ,   1,  1, 1 ]
-  count3d  = [IMT, JMT, KM, 1]
+  count1d  = [ km]
 
-  inquire(file=gridfile,exist=around)
-  if(.not.around) stop 4556
+  start2d  = [1, subGridImin, subGridJmin, 1 ]
+  count2d  = [1, subGridImax, subGridJmax, 1 ]
+  start3d  = [1, subGridImin, subGridJmin,  1]
+  count3d  = [1, subGridImax, subGridJmax, km]
 
+  ncTpos = 1
+  !Use  t=1  i=2  j=3
+  map2d    = [2, 3, 1, 1]
+  !Use  t=1  i=2  j=3  k=4
+  map3d    = [2, 3, 4, 1]   
 
-   ierr = NF90_OPEN (gridfile,NF90_NOWRITE,ncid)
-   IF (ierr /= 0) THEN
-    PRINT*,NF90_STRERROR (ierr)
-     END IF
-     
-   ierr = NF90_INQ_VARID (ncid,'ds_01_21_N',varid)
-   ierr = NF90_GET_VAR (ncid,varid,tem2d,start2d,count2d)
-   IF (ierr /= 0) THEN
-    PRINT*,NF90_STRERROR (ierr)
-    STOP
-   END IF
-   
-   dxv(1:imt,:)=tem2d
-   
-   
-   ierr = NF90_INQ_VARID (ncid,'ds_10_12_E',varid)
-   ierr = NF90_GET_VAR (ncid,varid,tem2d,start2d,count2d)
-   IF (ierr /= 0) THEN
-    PRINT*,NF90_STRERROR (ierr)
-    STOP
-   END IF   
-   
-   dyu(1:imt,:)=tem2d
-   dxdy = dxv(1:imt,:)*dyu(1:imt,:)
-  
+  dxv = int(floor(get2DfieldNC(trim(gridfile) , 'dxu')))
+  dyu = int(floor(get2DfieldNC(trim(gridfile) , 'dyu')))
+  dxv(1:imt,:) = cshift (dxv(1:imt,:), shift=79, dim=1)
+  dyu(1:imt,:) = cshift (dyu(1:imt,:), shift=79, dim=1)
+  dxdy = dxv*dyu
 
-  
   ! === Set up cell heights ===
-  
-  gridfile = trim(inDataDir) // 'monthlydata/ocean_month_20061231.nc'
-  print *,trim(gridfile)
-  
-   ierr = NF90_OPEN (gridfile,NF90_NOWRITE,ncid)
-   IF (ierr /= 0) THEN
-    PRINT*,NF90_STRERROR (ierr)
-     END IF
-     
-   ierr = NF90_INQ_VARID (ncid,'dht',varid)
-   ierr = NF90_GET_VAR (ncid,varid,tem3d,start3d,count3d)
-   IF (ierr /= 0) THEN
-    PRINT*,NF90_STRERROR (ierr)
-    STOP
-   END IF
-  
-  where (tem3d<-10000.) 
-   tem3d = 0.
-  end where
-
+  dzt = int(floor(get3DfieldNC(trim(gridfile) , 'dz_t')))
+  dzt = cshift (dzt, shift=79, dim=1)
+  dzu = int(floor(get3DfieldNC(trim(gridfile) , 'dz_c')))
+  dzu = cshift (dzu, shift=79, dim=1)
   do  k=1,km
-   dzt(:,:,km-k+1) =  tem3d(:,:,k)
+     dzt(:,:,km-k+1) =  dzt(:,:,k)
+     dzu(:,:,km-k+1) =  dzu(:,:,k)
   end do
-  
-  do i=1,IMT
-   ip=i+1
-   if(ip.eq.IMT+1) ip=1
-   do j=1,JMT
-    jp=j+1
-    if(jp.eq.JMT+1) jp=JMT ! there should be an f-fold here i think K.Döös
-     do k=1,KM
-      dzu(i,j,k) = min(dzt(i,j,k),dzt(ip,j,k))
-      dzv(i,j,k) = min(dzt(i,j,k),dzt(i,jp,k))
-!      if(k.eq.KM .and. i==1) print *,j, dzt(i,j,k), dzt(ip,j,k), dzu(i,j,k)
-     enddo
-    enddo
-   enddo
-! find the total depth
-depth=0.
- do k=1,KM
-  depth(:,:)=depth(:,:)+dzt(:,:,k)
- enddo
-
+  where (dzt>100000) 
+     dzt = 0
+  end where
+ where (dzu>100000) 
+     dzu = 0
+  end where
+  dzv = dzu
+  dzt0surf = dzu(:,:,km)
   ! === Load the bathymetry ===
-   ierr = NF90_INQ_VARID (ncid,'kmt',varid)
-   ierr = NF90_GET_VAR (ncid,varid,tem2d,start2d,count2d)
-   IF (ierr /= 0) THEN
-    PRINT*,NF90_STRERROR (ierr)
-    STOP
-   END IF  
-   
-  kmt=tem2d
-  where (kmt<-1000) 
-   kmt = 0
-  end where
+  depth = int(floor(get2DfieldNC(trim(gridfile) , 'depth_t')))
+  kmt = int(floor(get2DfieldNC(trim(gridfile) , 'kmt')))
+  depth = cshift (depth, shift=79, dim=1)
+  kmt = cshift (kmt, shift=79, dim=1)
 
-
-   ierr = NF90_INQ_VARID (ncid,'kmu',varid)
-   ierr = NF90_GET_VAR (ncid,varid,tem2d,start2d,count2d)
-   IF (ierr /= 0) THEN
-    PRINT*,NF90_STRERROR (ierr)
-    STOP
-   END IF  
-
-  kmu=tem2d
-  where (kmu<-1000) 
-   kmu = 0
-  end where
-  
-  
-   ierr = NF90_INQ_VARID (ncid,'kmv',varid)
-   ierr = NF90_GET_VAR (ncid,varid,tem2d,start2d,count2d)
-   IF (ierr /= 0) THEN
-    PRINT*,NF90_STRERROR (ierr)
-    STOP
-   END IF  
-
-  kmv=tem2d
-  where (kmv<-1000) 
-   kmv = 0
-  end where
-
-  
   mask = 1
   where (kmt==0) 
      mask = 0
   end where
-
-! Extra for extracting and testing variables which should be commented 
-!   ierr = NF90_INQ_VARID (ncid,'geolon_c',varid)
-!   ierr = NF90_GET_VAR (ncid,varid,geolon,start2d,count2d)
-!   IF (ierr /= 0) THEN
-!    PRINT*,NF90_STRERROR (ierr)
-!    STOP
-!   END IF  
-!   
-!   ierr = NF90_INQ_VARID (ncid,'geolat_c',varid)
-!   ierr = NF90_GET_VAR (ncid,varid,geolat,start2d,count2d)
-!   IF (ierr /= 0) THEN
-!    PRINT*,NF90_STRERROR (ierr)
-!    STOP
-!   END IF  
-!
-!open(21,file='/Users/doos/data/auscom/topo/longlat',form='unformatted')
-!write(21) geolon
-!write(21) geolat
-!write(21) kmt
-!write(21) kmu
-!close(21)
-!
-!do j=JMT,1,-1
-! write(*,"(370f13.3)") (geolat(i,j),i=1,10)
-! write(*,"(370i1)") (mask(i,j),i=1,IMT/2)
-!enddo 
-!
-!stop 3495
   
 end SUBROUTINE setupgrid
