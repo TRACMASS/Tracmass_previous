06909b4ec4a0e0a5a2470967e7c51be10623a5fb
Bror Jonsson <brorfred@gmail.com>
2015-11-24 13:28:12 -0500

    Rewind files rather than open new ones

--- src/writepsi.f95
+++ src/writepsi.f95
@@ -9,35 +9,33 @@ subroutine write_streamfunctions
 
 IMPLICIT none
 
+!call open_outfiles
+
 #ifdef streamxy
-open(51,file=trim(outDataDir)//trim(outDataFile)//'_psi_xy_yx.bin',form='unformatted')
+rewind(51)
 write(51) stxyy
 write(51) stxyx
-close(51)
-print *,'stream function streamxy written'
 #endif
 
 #if defined streamv
-open(52,file=trim(outDataDir)//trim(outDataFile)//'_psi_yz_xz.bin',form='unformatted')
+rewind(52)
 write(52) styz
 write(52) stxz
-close(52)
-print *,'stream function streamv written'
 #endif
 
 #if defined streamr 
-open(53,file=trim(outDataDir)//trim(outDataFile)//'_psi_xr_yr.bin',form='unformatted')
+rewind(53)
 write(53) stxr
 write(53) styr
-close(53)
-print *,'stream function streamr written'
 #endif
 
 #ifdef stream_thermohaline
-open(54,file=trim(outDataDir)//trim(outDataFile)//'_psi_ts.bin',form='unformatted')
+rewind(54)
 write(54) psi_ts
-close(54)
-print *,'stream function thermohaline written'
+#endif
+
+#if defined streamxy || streamv || streamr || stream_thermohaline
+print *,'stream function written'
 #endif
 
 
