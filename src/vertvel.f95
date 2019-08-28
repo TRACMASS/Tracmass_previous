59a1ac1f0a17bfd79a42fee1751d5f9c47925083
Kristofer Döös <doos@misu.su.se>
2019-08-28 09:53:04 +0200

    moved some old projects into the folder old

--- src/vertvel.f95
+++ src/vertvel.f95
@@ -61,8 +61,8 @@ subroutine vertvel(ia,iam,ja,ka)
 ! Make sure the vertical velocity is always zero at the ocean bottom and below as well as at the TOA
 #if defined atmospheric 
 wflux(0,:) = 0.d0
-#elif defined orca1 || orca12
-do k=0,KM-kmt(ia,ja)
+#elif defined orca1 || orca12  
+do k=0,KM-kmt(ia,ja)  ! Shouldn't this be the case for all models???
   wflux(k,:)=0.d0
 enddo
 #endif
